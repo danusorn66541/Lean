7bc1df98f06f7e91a8b645f08d78cb83f7bd57bc
Danusorn6678 <66022613@up.ac.th>
2026-06-20 00:22:19 +0700

    Fix measurementId for analytics

--- firebase-config.js
+++ firebase-config.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDESuXZqC4XM_VRt7XA2KPzObmolEiKXis",
-  authDomain: "lean-e8dfc.firebaseapp.com",
-  projectId: "lean-e8dfc",
-  storageBucket: "lean-e8dfc.firebasestorage.app",
-  messagingSenderId: "614716993747",
-  appId: "1:614716993747:web:93820ad104a22ae3be6426",
-  measurementId: "G-QD3TH1ZKYG"
+  apiKey: "AIzaSyDiNN9_E3YKF46b7RywW7lyczB3JP6mw_U",
+  authDomain: "lean-56e67.firebaseapp.com",
+  projectId: "lean-56e67",
+  storageBucket: "lean-56e67.firebasestorage.app",
+  messagingSenderId: "28737001937",
+  appId: "1:28737001937:web:171fd4f73058dcc586a8f0",
+  measurementId: "G-KWZLYDNRR4"
 };
 
 // Initialize Firebase
